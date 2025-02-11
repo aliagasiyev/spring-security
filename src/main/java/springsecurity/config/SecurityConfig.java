73308df394a87859e2a022f28bf5ae4812c05279
aliagasiyev <agasiyevali2@gmail.com>
2025-02-11 18:54:53 +0400

    Part-02 roll-base access is attached

--- src/main/java/springsecurity/config/SecurityConfig.java
+++ src/main/java/springsecurity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     // Bu metodun əsas xüsusiyyəti Spring Security üçün bir təhlükəsizlik filtri zənciri (SecurityFilterChain)
@@ -45,7 +47,7 @@ public class SecurityConfig {
                 build();
 
         UserDetails admin = User.withUsername("admin").
-                password("password").
+                password("{noop}password").
                 roles("ADMIN").
                 build();
         return new InMemoryUserDetailsManager(user1,admin);
--- src/main/java/springsecurity/controller/GreetingsController.java
+++ src/main/java/springsecurity/controller/GreetingsController.java
@@ -1,5 +1,6 @@
 package springsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingsController {
 
     @GetMapping("/say")
-    public  String sayHello(){
+    public String sayHello() {
         return "Hello World";
+    }
+
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String user() {
+        return "Hello From User";
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello From Admin";
     }
 }
